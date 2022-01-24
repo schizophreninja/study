5c92c974f2d7da7eddf1e0f694a36ba53f777f62
Schizophreninja <schizophreninja@gmail.com>
2022-01-24 21:13:30 +0500

    some changes

--- teapod/index.js
+++ teapod/index.js
@@ -1,50 +1,77 @@
 "use strict";
 
 const environment = {
-  teapot: new Teapot(1000, 300),
-
+  teapot: new Teapot(1000, 300, 0.1),
   human: new Human("Алексей"),
-
   physics: {
-    increaseHeat() {
-      return console.log("* чайник нагревается *");
-    },
-    reduceHeat() {
-      return console.log("* чайник остужается *");
+    coolWater: function () {
+      const coolRateInMilliseconds = 1000 * environment.coolRate_SecondsPerDegree;
+      if(environment.teapot.currentHeat <= environment.roomTemperature) return console.log("* вода остыла *");
+      environment.teapot.currentHeat--;
+      setTimeout(environment.physics.coolWater, coolRateInMilliseconds);
     }
-  }
+  },
+  waterAmount: 0,
+  roomTemperature: 25,
+  boilingHeat: 100,
+  coolRate_SecondsPerDegree: 0.1 // Скорость охлаждения воды 
 };
 
-function Teapot(capacity, minWater) {
-  if (!new.target) return new Teapot(capacity, minWater);
+function Teapot(capacity, minWater, heatRate_SecondsPerDegree) {
+  if (!new.target) return new Teapot(capacity, minWater, heatRate_SecondsPerDegree);
 
   this.properties = {
     capacity,
-    minWater
+    minWater,
+    heatRate_SecondsPerDegree,    // Скорость нагревания чайника, секунд на градус
   };
-  this.waterAmount = 0;
+
+  this.waterSensor = {
+    getWaterLevel: function () {
+      return environment.waterAmount;
+    } 
+  }
+
   this.currentHeat = 25;
   this.isEnabled = false;
+  this.heatTimer = null;
 
   this.changeState = function () {
     if (this.isEnabled) {
       this.isEnabled = false;
       console.log("Чайник выключен");
-      environment.physics.reduceHeat(); // replace
+      environment.physics.coolWater();
       return;
     }
-    if (this.waterAmount < this.properties.minWater)
+    if (this.waterSensor.getWaterLevel() < this.properties.minWater)
       return console.log("Добавьте воды в чайник");
     this.isEnabled = true;
+    this.startHeatUp();
     console.log("Чайник включен");
-    environment.physics.increaseHeat(); // replace
-  };
+  }
+
+  this.startHeatUp = function() {
+    this.heatTimer = setInterval(this.heatUp, 1000 * this.properties.heatRate_SecondsPerDegree, this);
+  }
+  this.heatUp = function (context) {
+    if(context.isEnabled == false) {
+      return;
+    }
+    if(context.currentHeat < environment.boilingHeat) {
+      context.currentHeat++;
+    }
+    else {
+      clearInterval(context.heatTimer);
+      console.log("* вода нагрелась *");
+      context.changeState();
+    }
+  }
 
   this[Symbol.toPrimitive] = function (hint) {
     return (
       `capacity:${this.properties.capacity}` +
       `, minWater:${this.properties.minWater}` +
-      `, waterAmount:${this.waterAmount}` +
+      `, waterAmount:${this.waterSensor.getWaterLevel()}` +
       `, currentHeat: ${this.properties.currentHeat}` +
       `, isEnabled: ${this.isEnabled}`
     );
@@ -61,8 +88,8 @@ function Human(name) {
   };
   this.adjustWater = function (amount) {
     const teapot = environment.teapot;
-    teapot.waterAmount +=
-      teapot.properties.capacity - teapot.waterAmount;
+    environment.waterAmount +=
+      teapot.properties.capacity - environment.waterAmount;
     console.log(`${this.name} добавил воды в чайник`);
   };
 }
